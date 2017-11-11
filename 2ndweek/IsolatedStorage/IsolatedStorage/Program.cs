fe0092d25ebba660d87273806a915ca98264930b
zsoltiNagy <zsoltiusz.nagy@gmail.com>
2017-11-11 17:07:50 +0100

    Isolated Storage SI 2nd week

---
+++ 2ndweek/IsolatedStorage/IsolatedStorage/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace IsolatedStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs");
+            userWriter.WriteLine("hablablablabla!4!");
+            userWriter.Close();
+            string[] files = userStore.GetFileNames("UserSettings.set");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("Didn't found any file matching your query.");
+                return;
+            }
+            userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+            StreamReader userReader = new StreamReader(userStream);
+            string contents = userReader.ReadToEnd();
+            Console.WriteLine(contents);
+        }
+    }
+}
