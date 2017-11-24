c79e41aa2ef78f175415efa8820c330fd3a178c9
zsoltiNagy <zsoltiusz.nagy@gmail.com>
2017-11-24 14:53:01 +0100

    ThreadingExercise1

---
+++ 3rdweek/Threading/Threading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Counting()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Count: {0} - Thread Id: {1}", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart starter = new ThreadStart(Counting);
+            Thread first = new Thread(starter);
+            Thread second = new Thread(starter);
+            Thread third = new Thread(starter);
+
+            first.Start();
+            second.Start();
+            third.Start();
+
+            first.Join();
+            second.Join();
+            third.Join();
+
+            Console.Read();
+        }
+    }
+}
