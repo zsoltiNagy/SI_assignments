928a63cece3ec26f28ec25008b0e64dee1423df7
zsoltiNagy <zsoltiusz.nagy@gmail.com>
2017-11-24 14:57:34 +0100

    ThreadPoolDemo

---
+++ 3rdweek/ThreadPoolDemo/ThreadPoolDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static public void ShowMyText(object state)
+        {
+            string txt = "State: " + (string)state;
+            txt += " Thread: " + Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(txt);
+        }
+
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.ReadKey();
+        }
+    }
+}
